5b3f6e2e671d1903b3eaead136c7a36d6acc111c
Jack <75438383+on-Sync@users.noreply.github.com>
2023-10-18 13:51:56 +0900

    Revert "사용자_컨텍스트 (#6)"
    This reverts commit f1161928e4e913ca9cee2e9cad2b47cab26ae306.

--- src/main/java/com/github/onsync/ecommerce/application/outbound/CreateUserPort.java
+++
@@ -1,8 +0,0 @@
-package com.github.onsync.ecommerce.application.outbound;
-
-import com.github.onsync.ecommerce.application.domain.User;
-
-public interface CreateUserPort {
-
-    User creatUser(User user);
-}
